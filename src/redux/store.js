29133acfbd82d4e9f64cf1696f03b3066b94e1e1
roshini-kotagiri <roshinikotagiri@gmail.com>
2024-06-17 21:08:51 +0530

    made few changes

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import studentTeacherReducer from './studentTeacherSlice';
-export const store=configureStore({
-    reducer:{
-        studentTeacher:studentTeacherReducer
-    }
-})
-export default store;
